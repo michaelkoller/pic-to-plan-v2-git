c5876a040c70d9df6469f75cedc0eed59c5f85a4
michaelkoller <koller_michael@gmx.net>
2019-10-13 15:08:06 +0200

    mcts for toy example done

--- pic_to_plan_v2/observation_trace_gen/test_uct_dag.py
+++ pic_to_plan_v2/observation_trace_gen/test_uct_dag.py
@@ -2,6 +2,10 @@ import math
 import random
 from graphviz import Digraph
 
+d_1 = 1
+d_2 = 0
+d_3 = 0
+
 class Node:
     nid = 0
     def __init__(self, state, in_edge):
@@ -11,6 +15,8 @@ class Node:
         self.in_edges = [in_edge]
         self.nid = Node.nid
         Node.nid += 1
+        self.mu_prime = None
+        self.n_prime = None
 
     def __repr__(self):
         return "ID " + str(self.nid) + " " + self.state
@@ -75,25 +81,6 @@ class Node:
     def get_lowest_possible_action_index(self):
         return min([e.possible_action_index for e in self.in_edges])
 
-    def get_best_child_node_plus_edge(self, exploration_value=1.4142135623730951, d_1=1, d_2=1, d_3=1):  # math.sqrt(2)
-        best_value = -math.inf
-        best_edges = []
-        for e in self.out_edges:
-            if e.num_visits == 0:
-                edge_value = math.inf
-            else:
-                # Saffidine, 2010, UCD
-                # edge_value = e.get_saff_mu(d_1) + \
-                #              exploration_value * math.sqrt(math.log(e.get_saff_p(d_2)) / e.get_saff_n(d_3))
-                edge_value = 1
-            if edge_value > best_value:
-                best_value = edge_value
-                best_edges = [e]
-            elif edge_value == best_value:
-                best_edges.append(e)
-        chosen_edge = random.choice(best_edges)
-        return chosen_edge.destination, chosen_edge
-
 class Edge:
     eid = 0
     def __init__(self, origin, destination, action, possible_action_index):
@@ -105,8 +92,8 @@ class Edge:
         Edge.eid += 1
         self.num_visits = 0
         self.total_reward = 0.0
-        self.mu_prime = 0
-        self.n_prime = 0
+        self.mu_prime = None
+        self.n_prime = None
 
     def __repr__(self):
         return "ID " + str(self.eid) + " " + str(self.action)
@@ -118,24 +105,62 @@ class Edge:
     def get_normal_mean_reward(self):
         return self.total_reward / self.num_visits if self.num_visits > 0 else math.inf
 
-    def get_saff_mu(self):
-        pass
+    def get_saff_mu(self, depth):
+        if depth == 0:
+            return self.get_normal_mean_reward()
+        else:
+            return ((self.mu_prime * self.n_prime) + sum(
+                [f.get_saff_mu(depth - 1) * f.num_visits for f in self.get_children_edges()])) / \
+                   (self.n_prime + sum([g.num_visits for g in self.get_children_edges()]))
 
-    def get_saff_n(self):
-        pass
+    def get_saff_n(self, depth):
+        if depth == 0:
+            return self.num_visits
+        else:
+            return self.n_prime + sum([f.get_saff_n(depth - 1) for f in self.get_children_edges()])
 
-    def get_saff_p(self):
-        pass
+    def get_saff_p(self, depth): #payoffs of b(x) (siblings of x plus x)
+        return sum([f.get_saff_n(depth) for f in self.get_siblings_plus_self()])
+
+    def get_siblings_plus_self(self):
+        return self.origin.out_edges
+
+    def get_children_edges(self):
+        return self.destination.out_edges
+
+    def get_best_child_node_plus_edge(self, exploration_value=1.4142135623730951, d_1=d_1, d_2=d_2, d_3=d_3):  # math.sqrt(2)
+        best_value = -math.inf
+        best_edges = []
+        for e in self.destination.out_edges:
+            if e.num_visits == 0:
+                edge_value = math.inf
+            else:
+                # Saffidine, 2010, UCD
+                edge_value = e.get_saff_mu(d_1) + \
+                              exploration_value * math.sqrt(math.log(e.get_saff_p(d_2)) / e.get_saff_n(d_3))
+            if edge_value > best_value:
+                best_value = edge_value
+                best_edges = [e]
+            elif edge_value == best_value:
+                best_edges.append(e)
+        chosen_edge = random.choice(best_edges)
+        return chosen_edge.destination, chosen_edge
 
 
 class UCT_Search:
     possible_actions = [(1, ["A+", "B+"]), (2, ["A+", "B+"]), (3, ["B-"]), (4, ["C+"])]
+    #possible_actions = [(1, ["A+"]), (2, ["A-"]), (3, ["A-"])]
     #possible_actions = [(1, ["A+", "B+", "C+", "D+"]), (2, ["A+", "B+", "C+"]), (3, ["A+", "B+"]), (4, ["A+"])]
     #possible_actions = [(1, ["A+"]), (2, ["B+"]), (3, ["C+"]), (4, ["D+"])]
     #possible_actions = [(1, ["A+"]), (2, ["B+"]), (3, ["A-"]), (4, ["C+"]), (5, ["B-"]), (6, ["D+"]), (7, ["C-"]), (8, ["D-"])]
-    d_1 = 1
-    d_2 = 1
-    d_3 = 1
+    # possible_actions = [(1, ["A+", "B+", "D+"]), \
+    #                     (2, ["A+", "B+", "D+"]), \
+    #                     (3, ["A+", "B+", "D+"]), \
+    #                     (4, ["A+", "B+", "D+"]), \
+    #                     (5, ["A+", "B+", "D+"]), \
+    #                     (6, ["A+", "B+", "D+"]), \
+    #                     (7, ["A+", "B+", "D+"])]
+    #possible_actions = [(1, ["A+", "B+", "D+"]), (2, ["A+", "B+", "D+"]), (3, ["A+", "B+", "D+"]), (4, ["B-"]), (5, ["C+"])]
 
     def __init__(self, initial_state):
         self.e_0 = Edge(None, None, None, -1)
@@ -154,10 +179,10 @@ class UCT_Search:
         while self.n_iter < self.n_iter_limit:
             #start new descent
             edge_descent_trace = []
-            tp_result_node, tp_result_edge_trace = self.tree_policy(self.n_0)
+            tp_result_node, tp_result_edge_trace = self.tree_policy(self.e_0)
             while True:
                 if tp_result_node.__hash__() in self.node_dict: #state already in DAG
-                    if len(tp_result_edge_trace) > 0:
+                    if len(tp_result_edge_trace) > 1:
                         #check for loop possibility
                         if self.check_if_reachable(self.node_dict[tp_result_node.__hash__()], tp_result_edge_trace[-1].origin):
                             #print("Detected loop creating action", tp_result_edge_trace[-1].origin, "-->", tp_result_edge_trace[-1], "-->", tp_result_node)
@@ -167,34 +192,44 @@ class UCT_Search:
                                     del tp_result_edge_trace[-1].origin.out_edges[i]
                             del tp_result_edge_trace[-1] #delete the loop creating edge from the backup path
                             edge_descent_trace += tp_result_edge_trace
-                            #BACKUP OR NO?
                             break
                         else:
                             #print("Detected DAG case", tp_result_edge_trace[-1].origin, "-->", tp_result_edge_trace[-1], "-->", tp_result_node)
                             tp_result_edge_trace[-1].destination = self.node_dict[tp_result_node.__hash__()]
                             self.node_dict[tp_result_node.__hash__()].in_edges.append(tp_result_edge_trace[-1])
                             edge_descent_trace += tp_result_edge_trace
-                            tp_result_node = self.node_dict[tp_result_node.__hash__()]
-                            tp_result_node, tp_result_edge_trace = self.tree_policy(tp_result_node)
+                            tp_result_node, tp_result_edge_trace = self.tree_policy(edge_descent_trace[-1])
                     else:
                         break
                 else:
                     #print("Detected new node", tp_result_edge_trace[-1].origin, "-->", tp_result_edge_trace[-1], "-->", tp_result_node)
                     self.node_dict[tp_result_node.__hash__()] = tp_result_node
                     edge_descent_trace += tp_result_edge_trace
-                    default_policy_result_value = self.default_policy() #TODO the mu_prime, n_prime should really be updated in the default_policy() --> also, then the mu_prime is just the saved value from the plan rec call
-                    edge_descent_trace[-1].mu_prime = default_policy_result_value
-                    edge_descent_trace[-1].n_prime = 1
                     break
 
-            if len(edge_descent_trace) > 0:
-                self.backup(edge_descent_trace, self.default_policy()) #TODO check in default policy, with the input of edge_descent_trace, if this is a case, where a loop has been avoided, then only backup from that state, if it is really a terminal state
+            if len(edge_descent_trace) > 1:
+                if edge_descent_trace[-1].destination.mu_prime is None:
+                    default_policy_result_value = self.default_policy(edge_descent_trace[-1].destination.state)
+                    edge_descent_trace[-1].destination.mu_prime = default_policy_result_value
+                    edge_descent_trace[-1].destination.n_prime = 1.0
+                if edge_descent_trace[-1].mu_prime is None:
+                    edge_descent_trace[-1].mu_prime = edge_descent_trace[-1].destination.mu_prime
+                    edge_descent_trace[-1].n_prime = edge_descent_trace[-1].destination.n_prime
+                    self.backup(edge_descent_trace, edge_descent_trace[-1].mu_prime)
+                elif edge_descent_trace[-1].destination.is_terminal():
+                    #found a dead state (if the dest node has still untried children left, you just remove a loop creating edge during this single descent.
+                    #but if the state is really dead, the terminal value should be backpropagated
+                    self.backup(edge_descent_trace, edge_descent_trace[-1].mu_prime)
+                for e in edge_descent_trace[:-1]:
+                    if e.mu_prime is None:
+                        e.mu_prime = e.destination.mu_prime
+                        e.n_prime = 1
             self.n_iter += 1
         print("Limit reached", self.n_iter, "/", self.n_iter_limit)
 
-    def tree_policy(self, tp_orig_node):
-        tp_result_edge_trace = []
-        tp_result_node = tp_orig_node
+    def tree_policy(self, tp_orig_edge):
+        tp_result_edge_trace = [tp_orig_edge]
+        tp_result_node = tp_orig_edge.destination
 
         while not tp_result_node.is_terminal():
             if not tp_result_node.is_fully_expanded():
@@ -203,12 +238,19 @@ class UCT_Search:
                     tp_result_edge_trace.append(expanded_e)
                 return expanded_n, tp_result_edge_trace
             else:
-                tp_result_node, e = tp_result_node.get_best_child_node_plus_edge()
+                tp_result_node, e = tp_result_edge_trace[-1].get_best_child_node_plus_edge()
                 tp_result_edge_trace.append(e)
         return tp_result_node, tp_result_edge_trace
 
-    def default_policy(self):
-        return 1
+    def default_policy(self, state):
+        val = 0.0
+        if "A" in state:
+            val += 0.3
+        if "B" in state:
+            val += 0.2
+        if "C" in state:
+            val += 0.1
+        return val
 
     def backup(self, edge_descent_trace, delta):
         for e in edge_descent_trace:
@@ -244,7 +286,7 @@ class UCT_Search:
         for e in node.out_edges:
             if e.eid not in ed:
                 ed[e.eid] = e
-                dot.edge(node.state, e.destination.state, e.action + "\nn:" + str(e.num_visits))
+                dot.edge(node.state, e.destination.state, e.action + "\nsmu:" + str(round(e.get_saff_mu(d_1),3)) + "\nmean:" + str(round(e.get_normal_mean_reward(),3)) + "\nn:" + str(e.num_visits) + "\nmu':" + str(round(e.mu_prime, 3)) + "\nn':" + str(e.n_prime))
                 self.viz_add_aux(dot, e.destination, nd, ed)
 
 
@@ -252,3 +294,4 @@ if __name__ == "__main__":
     uct_search = UCT_Search("")
     uct_search.search()
     uct_search.viz("test-viz")
+    print("Finished")
